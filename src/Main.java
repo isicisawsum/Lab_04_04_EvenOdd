871084118c1e3eaa7c4ed39c9f42d6e2da164e34
kregoric <75627074+isicisawsum@users.noreply.github.com>
2024-02-03 16:48:44 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter a number");
-        int numToExamine = 3;
-        System.out.println("You entered " + numToExamine);
-        System.out.println("Enter a second number");
-        int numToExameine2 = 2;
-        System.out.println("You entered " + numToExameine2);
-        int modOf = numToExamine % numToExameine2;
-        if (modOf == 0){
+        System.out.println("Enter a number"); //asks user to enter a number
+        int numToExamine = 3; //simulated input
+        System.out.println("You entered " + numToExamine); //confirms what user entered
+        System.out.println("Enter a second number"); //asks user to enter a second number
+        int numToExameine2 = 2; //second simulated input
+        System.out.println("You entered " + numToExameine2); //confirms what user entered
+        int modOf = numToExamine % numToExameine2; //calculations for variable modOf
+        if (modOf == 0){ //if then statement to calculate if number is even or odd
             System.out.println("Number is even");
         }   else {
             System.out.println("Number is odd");
